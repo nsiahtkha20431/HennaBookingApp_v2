dbdf192b79859cb4610d7ce6a2e4824f624fd9e4
nonsaucee <nishatkhan20431@htomail.ca>
2023-07-29 14:10:57 -0400

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment)
+
+const events = [
+  {
+    title: 'Henna Appointment',
+    start: new Date(2023, 7, 28, 10, 0), 
+    end: new Date(2023, 7, 28, 11, 0),
+  },
+];
+
+const Home = ({ events }) => {
+
+  const handleSlotSelection = (slotInfo) => {
+    const { start, end } = slotInfo;
+    console.log('Selected slot from', start, 'to', end);
+  };
+
+  return (
+    <div style={{ height: '500px' }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        views={['week']}
+        selectable="true"
+        step={60}
+        onSelectSlot={handleSlotSelection}
+      />
+    </div>
+  );
+
+};
+
+
+export default Home;
