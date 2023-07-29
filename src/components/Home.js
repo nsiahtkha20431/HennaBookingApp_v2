3ddde17b5ff07d44eaa1ef17d57ab8868b548765
nonsaucee <nishatkhan20431@htomail.ca>
2023-07-29 15:22:55 -0400

    Added time slot selection on front end - no backend functionality yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,42 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment)
 
-const events = [
-  {
-    title: 'Henna Appointment',
-    start: new Date(2023, 7, 28, 10, 0), 
-    end: new Date(2023, 7, 28, 11, 0),
-  },
-];
+// const events = [
+//   {
+//     title: 'Henna Appointment',
+//     start: new Date(2023, 7, 28, 10, 0), 
+//     end: new Date(2023, 7, 28, 11, 0),
+//   },
+// ];
 
-const Home = ({ events }) => {
+const Home = () => {
+
+  const [events, setEvents] = useState([
+    {
+      title: 'Henna Appointment',
+      start: new Date(2023, 7, 28, 10, 0),
+      end: new Date(2023, 7, 28, 11, 0),
+    },
+  ]);
+
+  // const handleSlotSelection = (slotInfo) => {
+  //   const { start, end } = slotInfo;
+  //   console.log('Selected slot from', start, 'to', end);
+  // };
 
   const handleSlotSelection = (slotInfo) => {
     const { start, end } = slotInfo;
+    const newEvent = {
+      title: 'New Appointment',
+      start: start,
+      end: end,
+    };
+  
+    setEvents([...events, newEvent]); // Add new event to the events array and update the state
+  
     console.log('Selected slot from', start, 'to', end);
   };
+  
 
   return (
     <div style={{ height: '500px' }}>
